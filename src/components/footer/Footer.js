063f9a9bf872ca355e63bd9b0013143003b8536e
Puneet <puneettiwari61@gmail.com>
2020-01-03 23:37:16 +0530

    update4

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,10 +4,10 @@ export default function Footer() {
   return (
       <footer className='footer'>
           <div className='connect'>
-          <a href='https://www.facebook.com/heyitspuneet' rel='noopener noreferrer' target='_blank'><i class="fab fa-facebook-square"></i></a>
-          <a href='https://twitter.com/puneettiwari61' rel='noopener noreferrer' target='_blank'><i class="fab fa-twitter-square"></i></a>
-          <a href='https://github.com/puneettiwari61' rel='noopener noreferrer' target='_blank'><i class="fab fa-github-square"></i></a>
-          <a href='https://medium.com/@puneettiwari61' rel='noopener noreferrer' target='_blank'><i class="fab fa-medium"></i></a>
+          <a href='https://www.facebook.com/heyitspuneet' rel='noopener noreferrer' target='_blank'><i className="fab fa-facebook-square"></i></a>
+          <a href='https://twitter.com/puneettiwari61' rel='noopener noreferrer' target='_blank'><i className="fab fa-twitter-square"></i></a>
+          <a href='https://github.com/puneettiwari61' rel='noopener noreferrer' target='_blank'><i className="fab fa-github-square"></i></a>
+          <a href='https://medium.com/@puneettiwari61' rel='noopener noreferrer' target='_blank'><i className="fab fa-medium"></i></a>
           </div>
           <small className='last'> &copy; 2020 Puneet Tiwari. All rights reserved.</small>
       </footer>
